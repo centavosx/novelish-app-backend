ee002b1a4a224e573f44b57e433a5db3c4478bb1
Centavosx <centavosx@Centavosxs-MacBook-Air.local>
2022-07-21 14:04:15 +0800

    Added follow author

--- controllers/author.js
+++ controllers/author.js
@@ -2,7 +2,7 @@ const Authors = require('../models/author')
 
 const { addImage } = require('./authorImages')
 const { isRequired } = require('./comments')
-
+const Books = require('../models/books')
 const { sendEmail } = require('../mail')
 
 const addAuthor = async (req, res) => {
@@ -55,6 +55,94 @@ const addAuthor = async (req, res) => {
   }
 }
 
+const getAuthorInformation = async (req, res) => {
+  try {
+    const authorData = await Authors.findOne(
+      { penName: req.query.author },
+      { name: 1, img: 1, bio: 1, followers: 1, penName: 1 }
+    )
+    if (!authorData)
+      return res
+        .status(403)
+        .json({ message: 'Author not found', tkn: req.tkn, rtkn: req.rtkn })
+    const books = await Books.aggregate([
+      {
+        $match: { bookAuthor: req.query.author },
+      },
+      {
+        $project: {
+          _id: 1,
+          bookName: 1,
+          bookAuthor: 1,
+          bookCoverImg: 1,
+          status: 1,
+          author: 1,
+          language: 1,
+          mainGenre: 1,
+          secondaryGenre: 1,
+          lastUpdated: 1,
+          description: 1,
+          chapterNumber: { $size: '$chapters' },
+          views: { $size: '$viewedBy' },
+          likes: { $size: '$likedBy' },
+        },
+      },
+      { $sort: { lastUpdated: -1 } },
+    ])
+
+    return res.json({
+      data: {
+        name: authorData.name,
+        img: authorData.img,
+        bio: authorData.bio,
+        followers: authorData.followers?.length ?? 0,
+        penName: authorData.penName,
+        books: books.length,
+      },
+      follow: authorData.followers.id(req.userId) ? true : false,
+      updated: books.slice(0, 3),
+    })
+  } catch (e) {
+    return res
+      .status(500)
+      .json({ message: e.message, tkn: req.tkn, rtkn: req.rtkn })
+  }
+}
+const getBooks = async (req, res) => {
+  try {
+    const books = await Books.aggregate([
+      {
+        $match: { bookAuthor: req.query.author },
+      },
+      {
+        $project: {
+          _id: 1,
+          bookName: 1,
+          bookAuthor: 1,
+          bookCoverImg: 1,
+          status: 1,
+          author: 1,
+          language: 1,
+          mainGenre: 1,
+          secondaryGenre: 1,
+          lastUpdated: 1,
+          description: 1,
+          chapterNumber: { $size: '$chapters' },
+          views: { $size: '$viewedBy' },
+          likes: { $size: '$likedBy' },
+        },
+      },
+      { $sort: { likes: -1 } },
+    ])
+
+    return res.json({
+      book: books.slice(req.query.start, req.query.end),
+    })
+  } catch (e) {
+    return res.status(500).json({ message: e.message })
+  }
+}
+
 const insertProfilePicture = async (req, res) => {
   try {
     if (!isRequired([req.file]))
@@ -77,8 +165,39 @@ const insertProfilePicture = async (req, res) => {
   }
 }
 
+const follow = async (req, res) => {
+  try {
+    const author = await Authors.findOne(
+      { penName: req.query.author },
+      { name: 1, img: 1, bio: 1, followers: 1, penName: 1 }
+    )
+    if (!author)
+      return res
+        .status(403)
+        .json({ message: 'Author not found', tkn: req.tkn, rtkn: req.rtkn })
+    if (author.followers.id(req.userId)) {
+      author.followers.id(req.userId).remove()
+    } else {
+      author.followers.push({ _id: req.userId })
+    }
+    await author.save()
+    return res.json({
+      author,
+      follow: author.followers.id(req.userId) ? true : false,
+      tkn: req.tkn,
+      rtkn: req.rtkn,
+    })
+  } catch (e) {
+    return res
+      .status(500)
+      .json({ message: e.message, tkn: req.tkn, rtkn: req.rtkn })
+  }
+}
+
 module.exports = {
   addAuthor,
-
+  getBooks,
   insertProfilePicture,
+  getAuthorInformation,
+  follow,
 }
--- routes/authors.js
+++ routes/authors.js
@@ -1,8 +1,17 @@
 const express = require('express')
-const { addAuthor } = require('../controllers/author')
+const {
+  addAuthor,
+  getAuthorInformation,
+  getBooks,
+  follow,
+} = require('../controllers/author')
 const { getImage, uploadImg } = require('../controllers/authorImages')
+const { authenticate } = require('../middlewares/authenticate')
 const router = express.Router()
 
 router.post('/:id', uploadImg, addAuthor)
 router.get('/images/:id', getImage)
+router.get('/', authenticate, getAuthorInformation)
+router.post('/', authenticate, follow)
+router.get('/books', getBooks)
 module.exports = router
